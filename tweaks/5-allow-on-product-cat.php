c7959c56c877653f8058157752e3813d6c7be6d9
Marin <contact@lueurcreative.fr>
2024-09-04 17:09:16 +0200

    🐛 FIX: Only show radio for edit or add page

--- tweaks/5-allow-on-product-cat.php
+++ tweaks/5-allow-on-product-cat.php
@@ -4,7 +4,15 @@
 
 function checktoradio()
 {
-  echo '<script type="text/javascript">jQuery("#product_catchecklist-pop input, #product_catchecklist input, .product_cat-checklist input").each(function(){this.type="radio"});</script>';
+  if (!get_post_type() !== "product") {
+    return;
+  }
+
+  $screen = get_current_screen();
+
+  if (($screen->parent_base === "edit" && $screen->action === "add") || ($screen->parent_base === "edit" && $_GET["action"] === "edit")) {
+    echo '<script type="text/javascript">jQuery("#product_catchecklist-pop input, #product_catchecklist input, .product_cat-checklist input").each(function(){this.type="radio"});</script>';
+  }
 }
 
 add_action('admin_footer', 'checktoradio');
