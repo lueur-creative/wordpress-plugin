a7ade4685bf41bbd78c2872bd901e1a3450c61bc
Marin <contact@lueurcreative.fr>
2024-09-12 14:24:31 +0200

    📦 NEW: Require plugins

--- lueur-creative.php
+++ lueur-creative.php
@@ -11,6 +11,17 @@ if (! defined('ABSPATH')) {
 
 define("LUEURCREATIVE_BASE_PATH", __DIR__ . "/");
 
+/** Import lib */
+
+require_once LUEURCREATIVE_BASE_PATH . "lib/class-tgm-plugin-activation.php";
+require_once LUEURCREATIVE_BASE_PATH . "lib/create-attributes.php";
+
+/** Import required plugins */
+
+require_once LUEURCREATIVE_BASE_PATH . "required-plugins.php";
+
+/** Import tweaks */
+
 require_once LUEURCREATIVE_BASE_PATH . "tweaks/0-add-product-image-notice.php";
 require_once LUEURCREATIVE_BASE_PATH . "tweaks/1-hide-hide-event-register.php";
 require_once LUEURCREATIVE_BASE_PATH . "tweaks/2-use-event-date-as-expiration.php";
@@ -21,5 +32,3 @@ require_once LUEURCREATIVE_BASE_PATH . "tweaks/6-make-product-image-square.php";
 require_once LUEURCREATIVE_BASE_PATH . "tweaks/7-add-metabox-product-duration.php";
 require_once LUEURCREATIVE_BASE_PATH . "tweaks/8-add-dashboard-metabox.php";
 require_once LUEURCREATIVE_BASE_PATH . "tweaks/9-pickup-work-hours-email-translation.php";
-
-require_once LUEURCREATIVE_BASE_PATH . "lib/create-attributes.php";
---
+++ required-plugins.php
@@ -0,0 +1,103 @@
+<?php
+
+function lueurcreative_register_required_plugins()
+{
+  /*
+     * Liste des plugins que vous voulez rendre obligatoires ou recommandés.
+     */
+  $plugins = [
+    [
+      'name'      => 'Advanced Local Pickup for WooCommerce',
+      'slug'      => 'advanced-local-pickup-for-woocommerce',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'Colissimo Shipping Methods for WooCommerce',
+      'slug'      => 'colissimo-shipping-methods-for-woocommerce',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'Contact Form 7',
+      'slug'      => 'contact-form-7',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'Flamingo',
+      'slug'      => 'flamingo',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'Jetpack',
+      'slug'      => 'jetpack',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'Jetpack boost',
+      'slug'      => 'jetpack-boost',
+    ],
+    [
+      'name'      => 'LiteSpeed Cache',
+      'slug'      => 'lightspeed-cache',
+    ],
+    [
+      'name'      => 'MailPoet',
+      'slug'      => 'mailpoet',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'Redis Cache',
+      'slug'      => 'redis-cache',
+    ],
+    [
+      'name'      => 'Sumup Payment Gateway for WooCommerce',
+      'slug'      => 'sumup-payment-gateway-for-woocommerce',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'Updraft Plus',
+      'slug'      => 'updraftplus',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'WooCommerce',
+      'slug'      => 'woocommerce',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'WooCommerce PDF Invoices',
+      'slug'      => 'woocommerce-pdf-invoices-packing-slips',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'WP Event Manager',
+      'slug'      => 'wp-event-manager',
+      'required'  => true,
+    ],
+    [
+      'name'      => 'WPSEOPress',
+      'slug'      => 'wp-seopress',
+      'required'  => true
+    ],
+    [
+      'name'      => 'WP SMTP',
+      'slug'      => 'wp-smtp',
+      'required'  => true,
+    ],
+  ];
+
+  /*
+     * Configuration des options de la classe TGM Plugin Activation.
+     */
+  $config = array(
+    'id'           => 'lueur-creative',                 // Identifiant unique pour TGM
+    'default_path' => '',                          // Chemin par défaut pour les plugins locaux
+    'menu'         => 'tgmpa-install-plugins',     // Slug de la page d'installation des plugins
+    'has_notices'  => true,                        // Afficher les notifications
+    'dismissable'  => false,                       // Les notifications peuvent être rejetées
+    'is_automatic' => false,                       // Plugin activé automatiquement après l'installation
+    'message'      => '',                          // Message en haut de la page TGMPA
+  );
+
+  tgmpa($plugins, $config);
+}
+add_action('tgmpa_register', 'lueurcreative_register_required_plugins');
